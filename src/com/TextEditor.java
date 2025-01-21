3a1c6a8dbbaf7b5954044368b5f30b0cfc77855b
Arikatakur <saleempay@hotmail.com>
2025-01-22 00:38:22 +0200

    add: create main window (feature #4)

--- src/com/TextEditor.java
+++
@@ -1,7 +0,0 @@
-package com; 
-
-public class TextEditor {
-    public static void main(String[] args) {
-        System.out.println("Text Editor application started!");
-    }
-}
---
+++ src/com/texteditor/TextEditor.java
@@ -0,0 +1,14 @@
+package com; 
+
+import javax.swing.*;
+
+public class TextEditor {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Text Editor");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        frame.setVisible(true);
+    }
+}
