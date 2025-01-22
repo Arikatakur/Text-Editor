f5487811be68b4cad88ca61abb4f528bafef941b
Saleem Yousef <130926254+Arikatakur@users.noreply.github.com>
2025-01-22 09:28:11 +0200

    feature: add text area (Closes #5)

--- src/main/java/com/texteditor/Main.java
+++ src/main/java/com/texteditor/Main.java
@@ -10,6 +10,11 @@ public class Main {
             System.out.println(MainConstants.HEADLESS_RUN);
         } else {
             JFrame frame = new JFrame("Text Editor");
+            JTextArea textArea = new JTextArea();
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            frame.setLayout(new BorderLayout());
+            frame.add(scrollPane, BorderLayout.CENTER);
+
             frame.setSize(800, 600);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
